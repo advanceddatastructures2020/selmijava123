98ee17f7bedb27ceeedd53a55e8ac85bb51834fd
selmi332 <76727323+selmi332@users.noreply.github.com>
2021-06-14 21:06:28 +0530

    Add files via upload

---
+++ sort.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+public class SortString
+{
+	public static void main(String args[])
+	{
+		//defining an array of type strings
+		String countries[] = {"Zimbabwe","South-Africa","India","America","Yugoslavia","Australia","Denmark","France"};
+		int size = countries.length;
+		//logic for sorting
+		for(int i=0; i<size-1; i++)
+		{
+			for(int j=i+1; j<countries.length; j++)
+			{
+				//countries each elements of the array to all the remaining elements
+				if (countries[i].compareTo(countries[j])>0)
+				{
+					//Swapping array elements
+					String temp = countries[i];
+					countries[i] = countries[j];
+					countries[j] = temp;
+				}
+			}
+		}
+		//prints the sorted array in ascending order
+		System.out.println(Arrays.toString(countries));
+	}
+}
